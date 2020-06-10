213b82c2f9dea5756b296e8122b1b77f7e8e9ccc
ctcoleman <ctcoleman510@gmail.com>
2020-06-10 17:20:15 -0400

    Finished task 6

--- index.js
+++ index.js
@@ -130,12 +130,14 @@ removeFlavorByName(originalFlavors, 'Vanilla')
 // 2 arguments 1 for your new array and one for your original array
 
 // and should return a new array that is identical to the old array. You can name the new array however you'd like. */
+function copy (arr, arrCopy) {
+  arrCopy = [...arr]
+  return arrCopy
+}
 
-// function copy( /* code here */ ) {
-
-//   /* code here */
-
-// }
+let originalFlavorsCopy = []
+originalFlavorsCopy = copy(originalFlavors, originalFlavorsCopy)
+console.log(originalFlavorsCopy)
 
 // /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
